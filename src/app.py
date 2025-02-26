8826458927074c69432d7af772b6e0bca5d0aac2
1122gs <GORDON.SMITHJR@hotmail.com>
2025-02-26 18:24:57 +0000

    final commit

--- src/app.py
+++ src/app.py
@@ -15,6 +15,24 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+
+John = {"id": jackson_family._generateId(), "first_name": "John","last_name":jackson_family.last_name,"age": 33, "lucky_numbers": [7, 3, 22]}
+Jane = {"id": 2, "first_name": "Jane","last_name":jackson_family.last_name, "age": 35, "lucky_numbers": [10, 14, 3]}
+Jimmy = {"id": 3, "first_name": "Jimmy","last_name":jackson_family.last_name, "age": 5, "lucky_numbers": [1]}
+
+jackson_family.add_member(John)
+jackson_family.add_member(Jane)
+jackson_family.add_member(Jimmy)
+
+# family_members = [
+#     {"id": 1, "first_name": "John", "age": 33, "lucky_numbers": [7, 3, 22]},
+#     {"id": 2, "first_name": "Jane", "age": 35, "lucky_numbers": [10, 14, 3]},
+#     {"id": 3, "first_name": "Jimmy", "age": 5, "lucky_numbers": [1]}
+# ]
+# for members in family_members:
+#   jackson_family.add_member(member)
+
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -24,6 +42,27 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
+@app.route('/member/int:<id>')
+def get_single_member(id):
+    member = jackson_family.get_member(id)
+    if member:
+        return jsonify(member), 200
+    return jsonify({"error": "Member not found"}), 404
+# status_code: 200 if success. 400 if bad request (wrong info). 500 if the server encounters an error
+
+@app.route('/member', methods=['POST'])
+def add_new_member ():
+    data = request.get_json()
+    member = jackson_family.add_member(data)
+    return jsonify(member),200
+
+
+@app.route('/member/int:<id>', methods=['DELETE'])
+def deleted_member():
+    data = jackson_family.delete_member(id)
+    return jsonify(data), 200
+
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
--- src/datastructures.py
+++ src/datastructures.py
@@ -11,8 +11,6 @@ from random import randint
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-
-        # example list of members
         self._members = []
 
     # read-only: Use this method to generate random members ID's when adding members into the list
@@ -21,15 +19,28 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        if "id" not in member:
+            member["id"] = self._generateId()
+        self._members.append(member)
+        return member
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == int(id):
+                self._members.pop(member)
+                return True
+        return False
+             
+    
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == int(id):
+              return member
+        return None
+        
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
